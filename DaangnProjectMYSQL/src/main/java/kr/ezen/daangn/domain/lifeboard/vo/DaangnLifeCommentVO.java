dd85763c69e5616c214547874b970e1e3806feee
kimc <tjdtlr12349@gmail.com>
2024-05-15 23:11:56 +0900

    asf

---
+++ DaangnProjectMYSQL/src/main/java/kr/ezen/daangn/domain/lifeboard/vo/DaangnLifeCommentVO.java
@@ -0,0 +1,38 @@
+package kr.ezen.daangn.domain.lifeboard.vo;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class DaangnLifeCommentVO {
+
+    private int idx;
+    
+    private int userRef;
+    
+    private int boardRef;
+    
+    private Integer parentComIdx; // 부모 댓글 인덱스 (null 가능)
+    
+    private String comment;
+    
+    private int likeCount;
+    
+    private int childCommentCount;
+    
+    private Integer isOwner;
+    
+    private LocalDateTime createDate;
+    
+    private LocalDateTime updateDate;
+    
+    private String ip;
+    
+    // 추가기입
+    private String nickName;
+	private String userProfile;
+	private int isLike;
+	private int isYours;
+    
+}
